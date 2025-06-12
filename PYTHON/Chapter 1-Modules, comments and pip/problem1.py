a5593c0c86644017eb1246aa5c26442b0ab13b54
Jai Kumar <jai21official@gmail.com>
2025-06-12 20:30:45 +0530

    Add files via upload

---
+++ PYTHON/Chapter 1-Modules, comments and pip/problem1.py
@@ -0,0 +1,5 @@
+print(''' bfgjkgkj,fgidhkj jnf gi ni
+      jngjn i ij jngi 
+      gfjn jnd jn gfgn
+       dnn gknknngs nknn
+       gdhh ''')
---
+++ PYTHON/Chapter 2-Variables and data types/problem-5.py
@@ -0,0 +1,3 @@
+a = int(input("write the value of a: "))
+b = int( input("write the value of b: "))
+print("average of a and b is", (a+b)/2)
