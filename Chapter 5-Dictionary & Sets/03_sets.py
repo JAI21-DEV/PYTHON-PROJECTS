99b94ea5f9c4ce8e226df03884822e7b900305ac
Jai Kumar <jai21official@gmail.com>
2025-06-14 14:39:59 +0530

    Delete Chapter 5-Dictionary & Sets directory

--- Chapter 5-Dictionary & Sets/03_sets.py
+++
@@ -1,8 +0,0 @@
-e = set() # this is an empty set
-# Dont use s = {} as it will create an empty dictionary
-s = {1, 5, 32, 54,5, 5, 5} 
-
-
-print(s)
-
-# we use sets when we don't want to repeat values from a set in our output
--- Chapter 5-Dictionary & Sets/problem-9.py
+++
@@ -1,5 +0,0 @@
-s = {8 , 7 , 5 , "jai" , [2 , 5]}
-
-# List can't be included in sets
-
-s[4][0] = 9
